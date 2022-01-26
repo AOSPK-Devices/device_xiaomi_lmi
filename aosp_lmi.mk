3324b26ca64f56ea6e8093e23dda8187b6fd0792
Alexandre Rangel <mamutal91@gmail.com>
2022-01-26 02:23:13 -0300

    lmi: Initial changes for Kraken
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Kraken stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := aosp_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
